22adc3dc2fdc16e343bb57ae7dc9d7c000874933
ildarfaz <ildarfazylbekov@gmail.com>
2023-07-20 23:48:46 +0500

    Initial commit

---
+++ src/components/content/Content.tsx
@@ -0,0 +1,11 @@
+import { Pagination } from "../pagination/Pagination"
+import { Table } from "../ui/table/Table"
+
+export const Content = () => {
+    return (
+        <div>
+            <Table />
+            <Pagination />
+        </div>
+    )
+}
---
+++ src/components/wrapper/Wrapper.tsx
@@ -0,0 +1,10 @@
+import { Content } from "../content/Content"
+import { Search } from "../search/Search"
+
+
+export const Wrapper = () => {
+    return <div>
+        <Search />
+        <Content />
+    </div>
+}
